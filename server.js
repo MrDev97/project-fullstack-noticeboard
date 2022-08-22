46999bd2c92447ac81ee6edf4f0702fdd93b3dca
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-22 18:17:02 +0200

    Create MongoDB cloud DB connection in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // initialize server
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const helmet = require('helmet');
 
 // import additional packages
@@ -33,6 +34,28 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('404 You shall not pass!');
 });
+
+const NODE_ENV = process.env.NODE_ENV;
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+let dbUri = '';
+
+if (NODE_ENV === 'production')
+  dbUri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@noticeboarddb.y1yzwgp.mongodb.net/NoticeBoardDB`;
+else if (NODE_ENV === 'test')
+  dbUri = 'mongodb://localhost:27017/NoticeBoardDBtest';
+else
+  dbUri = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@noticeboarddb.y1yzwgp.mongodb.net/NoticeBoardDB`;
+// connects our backend code with the database based on Node environment
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  if (NODE_ENV !== 'test') {
+    console.log('Connected to the database');
+  }
+});
+db.on('error', (err) => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   if (NODE_ENV !== 'test') {
