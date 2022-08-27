124d0c1815ee683d726d26403de20b6eb230c560
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-27 17:33:15 +0200

    Add authMiddleware to check if the user has open session or not / Block unauthorized requests

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../utils/authMiddleware');
 const auth = require('../controllers/auth.controller');
 
 router.post('/auth/register', auth.register);
 
 router.post('/auth/login', auth.login);
 
-router.get('/auth/user', auth.getUser);
+router.get('/auth/user', authMiddleware, auth.getUser);
 
 module.exports = router;
