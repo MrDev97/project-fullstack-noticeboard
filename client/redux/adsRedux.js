9d7397d51bee4749e00bfd7f6a04af6ee17df517
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-28 16:12:44 +0200

    Frontend - Update adsRedux with additional CRUD action creators

--- client/redux/adsRedux.js
+++ client/redux/adsRedux.js
@@ -9,12 +9,51 @@ export const getAdById = ({ ads }, adId) => ads.find((ad) => ad.id === adId);
 const reducerName = 'ads';
 const createActionName = (actionName) => `app/${reducerName}/${actionName}`;
 
+const START_REQUEST = createActionName('START_REQUEST');
+const END_REQUEST = createActionName('END_REQUEST');
+const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+
 const LOAD_ADS = createActionName('LOAD_ADS');
-const ADD_ADS = createActionName('ADD_ADS');
+const ADD_AD = createActionName('ADD_AD');
+const REMOVE_AD = createActionName('REMOVE_AD');
+const EDIT_AD = createActionName('EDIT_AD');
 
 // actions
+export const startRequest = (payload) => ({ payload, type: START_REQUEST });
+export const endRequest = (payload) => ({ payload, type: END_REQUEST });
+export const errorRequest = (payload) => ({ payload, type: ERROR_REQUEST });
+
 export const loadAds = (payload) => ({ payload, type: LOAD_ADS });
-export const addAd = (payload) => ({ payload, type: ADD_ADS });
+export const addAd = (payload) => ({ payload, type: ADD_AD });
+export const removeAd = (payload) => ({ payload, type: REMOVE_AD });
+export const editAd = (payload) => ({ payload, type: EDIT_AD });
+
+// thunks
+export const loadAdsRequest = () => {
+  return async (dispatch) => {
+    dispatch(startRequest({ name: 'LOAD_ADS' }));
+    try {
+      let res = await axios.get(`${API_URL}/ads`);
+      dispatch(loadAds(res.data));
+      dispatch(endRequest({ name: 'LOAD_ADS' }));
+    } catch (e) {
+      dispatch(errorRequest({ name: 'LOAD_ADS', error: e.message }));
+    }
+  };
+};
+
+export const addAdRequest = (ad) => {
+  return async (dispatch) => {
+    dispatch(startRequest({ name: 'ADD_AD' }));
+    try {
+      let res = await axios.post(`${API_URL}/ads`, ad);
+      dispatch(addAd(res.data));
+      dispatch(endRequest({ name: 'ADD_AD' }));
+    } catch (e) {
+      dispatch(errorRequest({ name: 'ADD_AD', error: e.message }));
+    }
+  };
+};
 
 // initial state
 const initialState = {
@@ -27,8 +66,44 @@ const adsReducer = (statePart = initialState, action = {}) => {
   switch (action.type) {
     case LOAD_ADS:
       return { ...statePart, data: [...action.payload] };
-    case ADD_ADS:
+    case ADD_AD:
       return { ...statePart, data: [...statePart.data, action.payload] };
+    case REMOVE_AD:
+      return statePart.filter((ad) =>
+        ad.id === action.payload ? false : true
+      );
+    case EDIT_AD:
+      return statePart.map((ad) =>
+        ad.id === action.payload.id ? { ...ad, ...action.payload } : ad
+      );
+    case START_REQUEST:
+      return {
+        ...statePart,
+        requests: {
+          ...statePart.requests,
+          [action.payload.name]: { pending: true, error: null, success: false },
+        },
+      };
+    case END_REQUEST:
+      return {
+        ...statePart,
+        requests: {
+          ...statePart.requests,
+          [action.payload.name]: { pending: false, error: null, success: true },
+        },
+      };
+    case ERROR_REQUEST:
+      return {
+        ...statePart,
+        requests: {
+          ...statePart.requests,
+          [action.payload.name]: {
+            pending: false,
+            error: action.payload.error,
+            success: false,
+          },
+        },
+      };
     default:
       return statePart;
   }
