54535f8041636e0dc898a9ab24ab39248e75dc49
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-29 22:29:41 +0200

    Add Search field in NavBar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Form, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 const NavBar = () => {
+  const [searchPhrase, setSearchPhrase] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // dispatch();
+    setSearchPhrase('');
+  };
+
+  useEffect(() => {
+    // dispatch();
+  }, [dispatch]);
+
   return (
     <Navbar bg='dark' variant='dark' expand='lg'>
       <Container>
@@ -14,11 +29,22 @@ const NavBar = () => {
           id='basic-navbar-nav'
           className='justify-content-end text-center'
         >
-          <Nav className='fs-4'>
+          <Nav className='fs-4 me-3'>
             <Nav.Link as={NavLink} to='/'>
               Home
             </Nav.Link>
           </Nav>
+          <Form onSubmit={handleSubmit} className='d-flex my-2 my-md-0'>
+            <Form.Control
+              type='search'
+              placeholder='Search'
+              className='me-2'
+              aria-label='Search'
+              value={searchPhrase}
+              onChange={(e) => setSearchPhrase(e.target.value)}
+            />
+            <Button variant='outline-success'>Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
