84e255f9c096734d862013034d85ad915a426b43
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-09-11 13:00:32 +0200

    Fix Redux DevTools dependency based on Environment

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -8,11 +8,8 @@ const subreducers = {
   users: usersReducer,
 };
 
-let reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__
-  ? window.__REDUX_DEVTOOLS_EXTENSION__()
-  : (f) => f;
-
-if (process.env.NODE_ENV === 'production') reduxDevTools = '';
+let reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'production') reduxDevTools = (f) => f;
 
 const reducer = combineReducers(subreducers);
 const store = createStore(
